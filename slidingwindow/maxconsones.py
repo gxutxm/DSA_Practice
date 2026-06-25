a7678db5cb0dd0168554bda63f780395aec2dbfe
Gautam G <gxutxm@gmail.com>
2026-06-24 23:02:57 -0400

    june 24

---
+++ slidingwindow/maxconsones.py
@@ -0,0 +1,34 @@
+'''
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+'''
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        current_ones = 0
+        max_ones = 0
+
+        for num in nums:
+            if num == 1:
+                current_ones += 1
+                max_ones = max(max_ones, current_ones)
+            else:
+                current_ones = 0
+
+        return max_ones
